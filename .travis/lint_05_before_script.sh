21c9de328261c945369291f8f49b49b075208a38
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2019-06-26 17:26:58 +0900

    disable linters

--- .travis/lint_05_before_script.sh
+++ .travis/lint_05_before_script.sh
@@ -4,6 +4,6 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-export LC_ALL=C
+# export LC_ALL=C
 
-git fetch --unshallow
+# git fetch --unshallow
